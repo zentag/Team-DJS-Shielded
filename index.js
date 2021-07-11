5ea92c7b63ea838f8b51ec36431f7f7aa715663e
zentag <zentagfw@gmail.com>
2021-07-11 00:31:07 -0400

    1.2
    Added Automod, Auto Error catch, and Connection Log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js') 
 const client = new Discord.Client();
 const WOKCommands = require('wokcommands');
+const mongo = require('./mongo.js')
 require('dotenv').config()
 // Hopefully REPL Stuff
 const express = require('express');
@@ -18,10 +19,98 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 global.botVersion = "1.0"
 
 client.on('ready', async () => {
+    try{
     console.log('ready')
+    sendConnectionLog()
     client.user.setActivity("My prefix is $", { type: "Playing"});
     new WOKCommands(client, {
         commandsDir: 'commands',
         featureDir: 'features'
     }).setDefaultPrefix('$').setMongoPath(process.env.mongoPath)
+    await mongo().then(mongoose => {
+        try{
+            console.log("Connected to mongo!")
+        }
+        finally{
+            mongoose.connection.close()
+        }
+    })
+    } catch(e) {
+        const errors = client.channels.cache.get("863631274001563651");
+            const errorEmbed = new Discord.MessageEmbed()
+                .setTitle(`Error While Starting`)
+                .addField("Stack Trace", e.stack.substring(0, 1024))
+                .setDescription(`Error: ${e}`)
+                .setColor("FF0000")
+            errors.send(errorEmbed)
+    }
 })
+
+function getTimestamp(){
+try {
+let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+    return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+} catch (e) {
+    const errors = client.channels.cache.get("863631274001563651");
+            const errorEmbed = new Discord.MessageEmbed()
+                .setTitle(`Error While Getting Timestamp`)
+                .addField("Stack Trace", e.stack.substring(0, 1024))
+                .setDescription(`Error: ${e}`)
+                .setColor("FF0000")
+            errors.send(errorEmbed)
+}
+}
+
+function sendConnectionLog() {
+    try {
+    const http = require('http');
+    var options = {
+        host: 'ipv4bot.whatismyipaddress.com',
+        port: 80,
+        path: '/'
+        };
+
+    http.get(options, function(res) {
+    console.log("status: " + res.statusCode);
+
+    res.on("data", function(chunk) {
+        const connections = client.channels.cache.get("863633306879787038");
+        const connEmbed = new Discord.MessageEmbed()
+            .setColor("E4D00A")
+            .setTitle("Bot Powered On")
+            .addField("IP Adress", chunk)
+            .addField("Timestamp", getTimestamp())
+        connections.send(connEmbed)
+    });
+    }).on('error', function(e) {
+        console.log("error: " + e.message);
+    });
+} catch (e) {
+    const errors = client.channels.cache.get("863631274001563651");
+            const errorEmbed = new Discord.MessageEmbed()
+                .setTitle(`Error While Getting IP Address`)
+                .addField("Stack Trace", e.stack.substring(0, 1024))
+                .setDescription(`Error: ${e}`)
+                .setColor("FF0000")
+            errors.send(errorEmbed)
+}
+}
