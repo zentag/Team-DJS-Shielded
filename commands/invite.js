719a333a738898fbaa59a45b9327447c6510471d
InfinityOofs <38821230+zentag@users.noreply.github.com>
2021-07-17 01:01:30 +0000

    2.1.1: mismatched name field

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,6 @@
 const config = require("../config.json")
 
 module.exports = { 
-    name: "support",
     description: " ':thumbsup:'",
     execute: (message) => {
        message.channel.send ("Invite me here: https://discord.com/api/oauth2/authorize?client_id=863465066308829184&permissions=469888087&scope=bot") 
