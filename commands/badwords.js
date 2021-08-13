4f315086e618f54297141a3a6266ec742fad300f
zentag <zentagfw@gmail.com>
2021-08-13 18:36:31 -0400

    2.3.1 badwords description

--- commands/badwords.js
+++ commands/badwords.js
@@ -1,6 +1,7 @@
 module.exports = {
     minArgs: 0,
     maxArgs: 0,
+    description: "Provides a webpage containing the list of all censored words",
     callback: ({ message }) => {
         message.reply(`This server's bad words can be found at https://team-djs-shielded.infinityoofs.repl.co/servers/${message.guild.id}/badwords`)
     }
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 exports.client = client
 
-global._botVersion = "2.3"
+global._botVersion = "2.3.1"
 global._globalEmbedFooter = `Shielded v${_botVersion}`
 
 client.login(process.env.token)
