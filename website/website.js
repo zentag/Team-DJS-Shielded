d57aa59588f7e876cdb75b264c832d578417e3a7
zentag <zentagfw@gmail.com>
2021-08-30 17:52:45 -0400

    website changes

--- website/website.js
+++ website/website.js
@@ -14,56 +14,56 @@ module.exports = {
             windowMs: 15 * 60 * 1000, // 15 minutes
             max: 50 // limit each IP to 100 requests per windowMs
         });
-        const httpsserver = https.createServer({
-            key: fs.readFileSync('/etc/letsencrypt/live/shielded.ddns.net/privkey.pem'),
-            cert: fs.readFileSync('/etc/letsencrypt/live/shielded.ddns.net/fullchain.pem'),
-          }, app);
+        app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+        // const httpsserver = https.createServer({
+        //     key: fs.readFileSync('/etc/letsencrypt/live/shielded.ddns.net/privkey.pem'),
+        //     cert: fs.readFileSync('/etc/letsencrypt/live/shielded.ddns.net/fullchain.pem'),
+        //   }, app);
         
-        httpsserver.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
-        httpsserver.get('/bad', (req, res) => res.sendFile(path.join(__dirname, '/badinput.html')));
-        httpsserver.get('/submit', (req, res) => res.sendFile(path.join(__dirname, '/submit.html')));
-        httpsserver.get('/scripts/style.css', (req, res) => res.sendFile(path.join(__dirname, '/style.css')));
-        httpsserver.get('/images/favicon', (req, res) => res.sendFile(path.join(__dirname, '/favicon.png')));
-        httpsserver.get('/.well-known/acme-challenge/VO81sqfImFl0gSRwbGMJKgphuzUjcq_rvg0xerNqhyo', (req, res) => res.sendFile(path.join(__dirname, '/VO81sqfImFl0gSRwbGMJKgphuzUjcq_rvg0xerNqhyo')))
+        // httpsserver.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+        // httpsserver.get('/bad', (req, res) => res.sendFile(path.join(__dirname, '/badinput.html')));
+        // httpsserver.get('/submit', (req, res) => res.sendFile(path.join(__dirname, '/submit.html')));
+        // httpsserver.get('/scripts/style.css', (req, res) => res.sendFile(path.join(__dirname, '/style.css')));
+        // httpsserver.get('/images/favicon', (req, res) => res.sendFile(path.join(__dirname, '/favicon.png')));
         
         httpsserver.listen(port, () => console.log(`Example httpsserver listening at http://localhost:${port}`));
         
-        httpsserver.use(express.urlencoded({ extended: false }));
-        httpsserver.use(express.static(path.join(__dirname, 'public')));
-        httpsserver.use(limiter);
-        httpsserver.engine('html', require('ejs').renderFile);
+        // httpsserver.use(express.urlencoded({ extended: false }));
+        // httpsserver.use(express.static(path.join(__dirname, 'public')));
+        // httpsserver.use(limiter);
+        // httpsserver.engine('html', require('ejs').renderFile);
         
-        httpsserver.post('/submit-form', function (req, res) {
-        const { channelID, channeltype, message, password } = req.body
-        if(channeltype !== "DM" && channeltype !== "GUILD") return res.sendFile(path.join(__dirname, '/badinput.html'));
-        if(password !== process.env.password0 && password !== process.env.password1) return res.sendFile(path.join(__dirname, '/submit.html'));
-        try{
-            if(channeltype == "DM") client.users.cache.get(channelID).send(message)
-            if(channeltype == "GUILD") client.channels.cache.get(channelID).send(message)
-        } catch(e){
-            res.sendFile(path.join(__dirname, '/badinput.html'))
-        }
-        res.sendFile(path.join(__dirname, '/submit.html'));
-        });
+        // httpsserver.post('/submit-form', function (req, res) {
+        // const { channelID, channeltype, message, password } = req.body
+        // if(channeltype !== "DM" && channeltype !== "GUILD") return res.sendFile(path.join(__dirname, '/badinput.html'));
+        // if(password !== process.env.password0 && password !== process.env.password1) return res.sendFile(path.join(__dirname, '/submit.html'));
+        // try{
+        //     if(channeltype == "DM") client.users.cache.get(channelID).send(message)
+        //     if(channeltype == "GUILD") client.channels.cache.get(channelID).send(message)
+        // } catch(e){
+        //     res.sendFile(path.join(__dirname, '/badinput.html'))
+        // }
+        // res.sendFile(path.join(__dirname, '/submit.html'));
+        // });
 
-        httpsserver.get('/servers/:id/badwords/confirmed', (req, res) => {
-            let badwords = "none"
-            mongo().then(async (mongoose) => {
-                const result = serverSettings.findOne({ guildId: req.params.id }, function (err, docs) {
-                    if (err){
-                        console.log(err)
-                    }
-                    else{
-                        if(docs && docs.badwords){
-                            badwords = docs.badwords.join(",badwords-seperator,")
-                        }
-                        res.render(path.join(__dirname, '/badwordspage.html'), {id:req.params.id,page:req.params.page,badwords:badwords})
-                    }
-                });
-            })
-        });
-        httpsserver.get('/servers/:id/badwords/', (req, res) => {
-            res.sendFile(path.join(__dirname, '/confirmbadwords.html'))
-        });
+        // httpsserver.get('/servers/:id/badwords/confirmed', (req, res) => {
+        //     let badwords = "none"
+        //     mongo().then(async (mongoose) => {
+        //         const result = serverSettings.findOne({ guildId: req.params.id }, function (err, docs) {
+        //             if (err){
+        //                 console.log(err)
+        //             }
+        //             else{
+        //                 if(docs && docs.badwords){
+        //                     badwords = docs.badwords.join(",badwords-seperator,")
+        //                 }
+        //                 res.render(path.join(__dirname, '/badwordspage.html'), {id:req.params.id,page:req.params.page,badwords:badwords})
+        //             }
+        //         });
+        //     })
+        // });
+        // httpsserver.get('/servers/:id/badwords/', (req, res) => {
+        //     res.sendFile(path.join(__dirname, '/confirmbadwords.html'))
+        // });
     }
 }
